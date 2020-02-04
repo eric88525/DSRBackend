54216f8544da99a93518aac052d781d518bf4aaf
eric88525 <eric885251@gmail.com>
2020-02-04 18:23:16 +0800

    backend-userlogin backend-migrate table "Projects" done

---
+++ database/migrations/2020_02_02_145614_create_projects_table.php
@@ -0,0 +1,49 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProjectsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+
+        Schema::create('projects', function (Blueprint $table) {
+            $table->string('cn-customerName')->nullable($value = true);
+            $table->string('en-customerName')->nullable($value = true);
+            $table->string('region')->nullable($value = true);
+            $table->string('sales')->nullable($value = true);
+            $table->string('programName')->nullable($value = true);
+            $table->string('prodLine')->nullable($value = true);
+            $table->string('partNumber')->nullable($value = true);
+            $table->string('projectUnits')->nullable($value = true);
+            $table->string('qtyBoard')->nullable($value = true);
+            $table->string('asp')->nullable($value = true);
+            $table->string('amount')->nullable($value = true);
+            $table->string('confidencePercent')->nullable($value = true);
+            $table->string('productionDate')->nullable($value = true);
+            $table->string('supportNeeded')->nullable($value = true);
+            $table->string('update')->nullable($value = true);
+            $table->string('top10')->nullable($value = true);
+            $table->string('opportunity')->nullable($value = true);
+            $table->string('createdDate')->nullable($value = true);
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('projects');
+    }
+}
---
+++ database/migrations/2020_02_04_091251_create_tests_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTestsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tests', function (Blueprint $table) {
+            $table->string('customerNameCn')->nullable($value = true);
+            $table->string('customerNameEn')->nullable($value = true);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tests');
+    }
+}
